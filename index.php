d2de5616846b1a62fddbd72dcbfd2aa513c9312e
RickyLoader <loaderricky123@gmail.com>
2019-03-12 15:37:00 +1300

    initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+
+require 'src/routes/dectalk.php';
+$app->run();
---
+++ src/routes/dectalk.php
@@ -0,0 +1,19 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app->get('/api/say/{tts}',function(Request $request, Response $response){
+    $tts = $request->getAttribute('tts');
+    $command = "say.exe -w tts.wav "."\"[:phoneme on]\" "."\"".$tts."\"";
+    shell_exec("cd ../dectalk && ".$command);
+    $file = "../dectalk/tts.wav";
+    downloadFile($file);
+});
+
+function downloadFile($file){
+    header('Content-Type: application/octet-stream');
+    header("Content-Transfer-Encoding: Binary"); 
+    header("Content-disposition: attachment; filename=\"".basename($file)."\""); 
+    readfile($file);
+    unlink($file);
+}
